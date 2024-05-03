c843ce0c3c9ae732ad84132c0614e2bc243a9671
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-05-03 12:34:52 +0200

    navigation  bar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 
-import './App.css'
+import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
- 
+
 
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <h1>Go Green</h1>
+          <Navbar />
         </header>
       </div>
-    
+
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+// NavBar.js
+
+import React from 'react';
+import './NavBar.css'; // Importing the CSS file for styling
+import logo from '../assets/logo.png'; // Importing the logo image
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="Logo" className="logo-img" />
+        </div>
+        <ul className="nav-links">
+        <li className="nav-item">
+            <a href="#charging-solutions" className="nav-link">Charging Solutions</a>
+            <div className="dropdown-content">
+              <a href="#">Solution 1</a>
+              <a href="#">Solution 2</a>
+              <a href="#">Solution 3</a>
+            </div>
+          </li>
+
+
+
+          <li className="nav-item">
+            <a href="#products" className="nav-link">Products</a>
+          </li>
+
+         
+          <li className="nav-item">
+            <a href="#contact-us" className="nav-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
