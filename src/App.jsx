d97c2f95cb45cafd806c8fc19c785d5d6fdb343f
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-05-03 11:32:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Go Green</h1>
+        </header>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
