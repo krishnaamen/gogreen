edffdf5ff25a0be570323327f0c6847fb728724c
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-05-13 15:25:55 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,14 @@ function App() {
           <Navbar />
         </header>
 
-
-
-
         <div className="bodycontent">
           <div className='contactdiv'>
             <ContactUs />
           </div>
 
           <div className='imagediv'>
-            <h5>Go GreenSolar Charging</h5>
+            <h6 className='pgtext'>What Do We Focus On
+              Reliable Chargers & Outstanding Service</h6>
             <div className='pgtext'>GoGreenSolar Charger prioritizes two things above all: top-tier product quality and exceptional customer service. Our commitment to building lasting partnerships with our customers is unmatched. By consistently setting and surpassing industry standards, we ensure every GoGreenSolar charger offers reliability and efficiency. Our comprehensive suite of services complements our product range, ensuring our customers' satisfaction remains unmatched.</div>
 
             <img className='bg-image' src={img} alt="img" />
